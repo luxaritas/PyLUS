980e37f915f444a075418caba2e1bebeb84f5b35
Avery <averysumner@gmail.com>
2018-04-06 21:54:49 -0700

    nightly, added more char create stuff, messed with sessions

--- cms/game/models.py
+++ cms/game/models.py
@@ -2,16 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Session(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     token = models.CharField(max_length=32)
     created = models.DateTimeField(auto_now=True)
+    address = models.IntegerField()
+    objid = models.BigIntegerField()
 
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    session = models.OneToOneField(Session, on_delete=models.CASCADE)
+    lego_club = models.BooleanField()
+    free_to_play = models.BooleanField()
+    new_subscriber = models.BooleanField()
+    front_character = models.SmallIntegerField()
 
 class Character(models.Model):
-    id = models.IntegerField(primary_key=True)
-    user_id = models.IntegerField(primary_key=True)
-    character_name = models.CharField(max_length=33)
-    character_unapproved_name = models.CharField(max_length=33)
+    slot = models.SmallIntegerField()
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    name = models.CharField(max_length=33)
+    unapproved_name = models.CharField(max_length=33)
     is_name_rejected = models.BooleanField()
     shirt_color = models.IntegerField()
     shirt_style = models.IntegerField()
