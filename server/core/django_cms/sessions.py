07a102bb3c01e7c439f9578b597721b17ab697ac
Knightoffaith <knightoffaith6@gmail.com>
2018-05-23 14:59:38 +0000

    Refactor sessions and authentication

--- server/core/django_cms/sessions.py
+++ server/core/django_cms/sessions.py
@@ -16,16 +16,21 @@ class SessionManager(Plugin):
     """
     Session manager plugin
     """
+    
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.session_cache = []
+    
     def actions(self):
         """
         Returns all actions
         """
         return [
             Action('session:new_session', self.new_session, 10),
-            Action('session:get_session', self.get_session, 10),
-            Action('session:set_clone', self.set_clone, 10),
             Action('pkt:session_info', self.verify_session, 10),
             Action('rn:user_packet', self.allow_packet, 10),
+            Action('session:get_session', self.get_session, 10),
+            Action('session:set_clone', self.set_clone, 10),
         ]
 
     def packets(self):
@@ -36,62 +41,30 @@ class SessionManager(Plugin):
             SessionInfo
         ]
 
-    def new_session(self, address, uid):
+    def new_session(self, account, address):
         """
         Creates a new session
         """
         token = secrets.token_urlsafe(24)
-        hashed = bcrypt.hashpw(token.encode('latin1'), bcrypt.gensalt())
-
-        try:
-            account = Account.objects.get(user__id=uid)
-        except Account.DoesNotExist:
-            raise KeyError(f'Invalid user ID: {uid}')
+        hashed = bcrypt.hashpw(token, bcrypt.gensalt())
 
         session = Session(account=account, ip=address[0], port=address[1], token=hashed)
         session.save()
 
         return token
-
-    def get_session(self, address):
-        """
-        Returns a user session
-        """
-        try:
-            return Session.objects.get(ip=address[0], port=address[1])
-        except Session.DoesNotExist:
-            return None
-
-    def check_token(self, token, hashed):
-        """
-        Checks a session token
-        """
-        return bcrypt.checkpw(token if isinstance(token, bytes) else token.encode('latin1'),
-                              hashed if isinstance(hashed, bytes) else hashed.encode('latin1'))
-
-    def destroy_session(self, address):
-        """
-        Destroys a session
-        """
-        Session.objects.filter(ip=address[0], port=address[1]).delete()
-
-    def set_clone(self, address, clone):
-        """
-        Sets the world clone for a session
-        """
-        session = Session.objects.get(ip=address[0], port=address[1])
-        session.clone = clone
-        session.save()
-
+    
     def verify_session(self, packet, address):
         """
         Verifies a session
         """
-        session = self.get_session(address)
+        try:
+            session = Session.objects.get(ip=address[0], port=address[1])
+            self.session_cache[address] = session
+        except Session.DoesNotExist:
+            session = None
 
-        if not session or not self.check_token(packet.session_key, session.token):
+        if not session or not bcrypt.checkpw(packet.session_key, session.token):
             self.destroy_session(address)
-            self.server.rnserver.close_connection(address)
 
     def allow_packet(self, data, address):
         """
@@ -101,7 +74,27 @@ class SessionManager(Plugin):
 
         if not getattr(packet, 'allow_without_session') and not self.get_session(address):
             self.destroy_session(address)
-            self.server.rnserver.close_connection(address)
+    
+    def get_session(self, address):
+        """
+        Returns a user session
+        """
+        return self.session_cache.get(address)
+
+    def destroy_session(self, address):
+        """
+        Destroys a session
+        """
+        Session.objects.filter(ip=address[0], port=address[1]).delete()
+        self.server.rnserver.close_connection(address)
+
+    def set_clone(self, address, clone):
+        """
+        Sets the world clone for a session
+        """
+        session = Session.objects.get(ip=address[0], port=address[1])
+        session.clone = clone
+        session.save()
 
 
 class SessionInfo(Packet):
