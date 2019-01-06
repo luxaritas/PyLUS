309a594108fa2e376c624e78ed394e6a47d69a65
Knightoffaith <knightoffaith6@gmail.com>
2019-01-06 15:55:33 -0500

    Typo fix

--- server/world/world_redirect.py
+++ server/world/world_redirect.py
@@ -12,7 +12,7 @@ class WorldRedirect(Plugin):
     """
     World manager class
     """
-    
+
     def actions(self):
         """
         Returns list of actions
@@ -20,7 +20,7 @@ class WorldRedirect(Plugin):
         return [
             Action('pkt:join_world_request', self.join_world_request, 10),
         ]
-    
+
     def packets(self):
         """
         Returns list of packets
@@ -28,7 +28,7 @@ class WorldRedirect(Plugin):
         return [
             JoinWorldRequest,
         ]
-    
+
     def join_world_request(self, packet, address):
         """
         Handles initial redirect from char list
@@ -36,13 +36,13 @@ class WorldRedirect(Plugin):
         session = self.server.handle_until_return('session:get_session', address)
         # One of the plugins listening is expected to set session.character
         self.server.handle('session:char_selected', session, packet.character_id)
-        
+
         last_zone = self.server.handle_until_return('char:initial_zone', session.character)
         zone_conf = self.server.config['servers'][ZONE_NAMES[last_zone]]
-        
+
         res = RedirectToServer(zone_conf['public_host'], zone_conf['public_port'])
         self.server.rnserver.send(res, address)
-    
+
 class JoinWorldRequest(Packet):
     """
     Join world request packet
@@ -59,18 +59,18 @@ class JoinWorldRequest(Packet):
         """
         char_id = stream.read(c_int64)
         return cls(char_id)
-        
+
 class RedirectToServer(Packet):
     """
     Redirects client to a new server instance
     """
     packet_name = 'redirect_to_server'
-    
-    def __init__(self, ip, port, mythran_notifcation=False):
+
+    def __init__(self, ip, port, mythran_notification=False):
         super().__init__(**{k: v for k, v in locals().items() if k != 'self'})
-        
+
     def serialize(self, stream):
         super().serialize(stream)
         stream.write(CString(self.ip, allocated_length=33))
         stream.write(c_uint16(self.port))
-        stream.write(c_bool(self.mythran_notifcation))
+        stream.write(c_bool(self.mythran_notification))
