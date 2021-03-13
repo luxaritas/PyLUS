432b0a0d257e8976052c82db22b915ad46d0ba95
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2021-03-13 14:12:09 -0600

    Migrated to new config.
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -11,14 +11,18 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import yaml
+# import yaml
+from util import MainConfig
 
-try:
-    with open('config.yml') as f:
-        user_config = yaml.safe_load(f)
-except FileNotFoundError:
-    with open('config.default.yml') as f:
-        user_config = yaml.safe_load(f)
+user_config = MainConfig()
+user_config.load()
+
+# try:
+#     with open('config.yml') as f:
+#         user_config = yaml.safe_load(f)
+# except FileNotFoundError:
+#     with open('config.default.yml') as f:
+#         user_config = yaml.safe_load(f)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,16 +32,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if user_config['cms']['secret_key'] == 'generate':
+# if user_config['cms']['secret_key'] == 'generate':
+if user_config.cms.secret_key == 'generate':
     from django.utils.crypto import get_random_string
     SECRET_KEY = get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
 else:
-    SECRET_KEY = user_config['cms']['secret_key']
+    # SECRET_KEY = user_config['cms']['secret_key']
+    SECRET_KEY = user_config.cms.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = user_config['cms']['debug']
+# DEBUG = user_config['cms']['debug']
+DEBUG = user_config.cms.debug
 
-ALLOWED_HOSTS = [user_config['cms']['public_host']]
+# ALLOWED_HOSTS = [user_config['cms']['public_host']]
+ALLOWED_HOSTS = [user_config.cms.public_host]
 
 # Application definition
 
@@ -136,5 +144,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-if not user_config['cms']['debug']:
+# if not user_config['cms']['debug']:
+if not user_config.cms.debug:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
